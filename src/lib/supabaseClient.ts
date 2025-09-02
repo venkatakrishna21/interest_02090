d4e89de016bef78adc0d28bbb9ac9a3e1f1bdb86
venkatakrishna21 <70277422+venkatakrishna21@users.noreply.github.com>
2025-09-02 22:45:34 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,12 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+supabase.auth.signInWithOtp({
+  email,
+  options: {
+    emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+  },
+});
+
