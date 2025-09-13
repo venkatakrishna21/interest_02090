247e2d94b069722245c8fe057a743ec446d6e54b
venkatakrishna21 <70277422+venkatakrishna21@users.noreply.github.com>
2025-09-13 16:27:12 +0530

    Update page.tsx

--- src/app/dashboard/owner/page.tsx
+++ src/app/dashboard/owner/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import OwnerDashboard from "@/components/OwnerDashboard";
 
-export default function OwnerDashboardPage() {
+export default function OwnerPage() {
   return <OwnerDashboard />;
 }
